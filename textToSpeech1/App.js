eeba209db69f63b8fadb4d65d8006ef4b4b36f56
lottemakinen <lotte.makinen@myy.haaga-helia.fi>
2020-11-06 14:28:06 +0200

    Exercise: Text to speech

---
+++ textToSpeech1/App.js
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, Button, TextInput } from 'react-native';
+import Constants from 'expo-constants';
+import * as Speech from 'expo-speech';
+
+export default class App extends React.Component {
+
+
+  speak() {
+    var thingToSay = 'Hello world';
+    Speech.speak(thingToSay);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput style= {{margin: 25, width: 300, borderColor: 'black', borderWidth: 1, }}/>
+        <Button title="Press to hear some words" onPress={this.speak} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
